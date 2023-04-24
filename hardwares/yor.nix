4d64236663740efd4e444717ea4038acba4e9916
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-04-24 14:32:20 +0200

    yor: mention about bluetooth

--- hardwares/yor.nix
+++ hardwares/yor.nix
@@ -44,4 +44,7 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # no adapter
+  # hardware.bluetooth.enable = true;
 }
