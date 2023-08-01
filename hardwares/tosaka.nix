ba16640110e810c12dd6565a924343ece6da869e
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-08-01 10:50:46 +0200

    tosaka: enable nvidia modsetting

--- hardwares/tosaka.nix
+++ hardwares/tosaka.nix
@@ -47,4 +47,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.nvidia.modesetting.enable = true;
 }
