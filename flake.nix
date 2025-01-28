0c7c36d3038c0cd38257c47b5796e43aef970ca4
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2025-01-28 15:07:53 +0100

    sqlc: v1.28.0

--- flake.nix
+++ flake.nix
@@ -10,6 +10,16 @@
         dbeaver-bin = prev.dbeaver-bin.override { override_xmx = "4096m"; };
         code-cursor = final.callPackage ./packages/code-cursor { };
         hcp = final.callPackage ./packages/hcp { };
+        sqlc = prev.sqlc.overrideAttrs (old: {
+          version = "1.28.0";
+          src = final.fetchFromGitHub {
+            owner = "sqlc-dev";
+            repo = "sqlc";
+            rev = "v1.28.0";
+            hash = "sha256-kACZusfwEIO78OooNGMXCXQO5iPYddmsHCsbJ3wkRQs=";
+          };
+          vendorHash = "sha256-5KVCG92aWVx2J78whEwhEhqsRNlw4xSdIPbSqYM+1QI=";
+        });
       };
 
       # helper function to make NixOS systems with common config
