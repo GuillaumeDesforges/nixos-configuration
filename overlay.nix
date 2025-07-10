d5441c46a81f12be6027d9169be498831b0fc3fc
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2025-07-10 18:10:29 +0200

    pkg(sqlc): 1.28 -> 1.29

--- overlay.nix
+++ overlay.nix
@@ -6,15 +6,4 @@ final: prev: {
   exhaustruct = final.callPackage ./packages/exhaustruct { };
 
   hcp = final.callPackage ./packages/hcp { };
-
-  sqlc = prev.sqlc.overrideAttrs (old: {
-    version = "1.28.0";
-    src = final.fetchFromGitHub {
-      owner = "sqlc-dev";
-      repo = "sqlc";
-      rev = "v1.28.0";
-      hash = "sha256-kACZusfwEIO78OooNGMXCXQO5iPYddmsHCsbJ3wkRQs=";
-    };
-    vendorHash = "sha256-5KVCG92aWVx2J78whEwhEhqsRNlw4xSdIPbSqYM+1QI=";
-  });
 }
