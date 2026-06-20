5b03090b0ecae52b3857634fdd3d58b1e30abfd0
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2026-06-20 10:56:05 +0200

    flake: bump to nixos 26.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-  inputs.home-manager.url = "github:nix-community/home-manager/release-25.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  inputs.home-manager.url = "github:nix-community/home-manager/release-26.05";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.claude-code.url = "github:sadjow/claude-code-nix";
